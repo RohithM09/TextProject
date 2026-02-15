999725cf90a175bafc81e5ce317373c3bb956cbb
RohithM09 <rohithmidde21@gmail.com>
2026-02-15 06:34:27 +0530

    dark mode

---
+++ src/components/TextForm.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+export default function TextForm({ heading }) {
+  const handleUpClick = () => {
+    //console.log("Uppercase was clicked" + text.toUpperCase());
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    let newtext = text.toLowerCase();
+    setText(newtext);
+  };
+
+  const handle = () => {
+    let newtext = " ";
+    setText(newtext);
+  };
+
+  const handleCopy = () => {
+    console.log("I am Copy");
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleOnChange = (event) => {
+    //console.log("On Change");
+    setText(event.target.value);
+  };
+
+  const capitalize = () => {
+    let newtext = text[0].toUpperCase();
+    setText(newtext + text.slice(1));
+  };
+
+  const [text, setText] = useState("");
+  //text = "new text";
+
+  return (
+    <>
+      <div className="container">
+        <h1>{heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to toLowerCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={capitalize}>
+          Capitalize
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handle}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Copy Text
+        </button>
+      </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{text.split(" ").length * 0.008} Minutes</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
---
+++ src/module2.mjs
@@ -0,0 +1,9 @@
+const a = "Harry";
+const b = "rohan";
+const c = "Akash";
+const d = "Priyanka";
+
+export default d;
+export { a };
+export { c };
+export { d };
